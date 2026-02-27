0c949f2d00fcb6532f56bec245d59a8614782ac7
alexandrendev <alxfn86@gmail.com>
2026-02-27 20:29:35 -0300

    feat: crud de categorias

---
+++ lista-tarefas-laravel/src/resources/views/categorias/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('title', 'Detalhes da categoria')
+
+@section('content')
+<div>
+    <div><span>Nome:</span> {{ $categoria->nome }}</div>
+    <div><span>Cor:</span> {{ $categoria->cor }}</div>
+    <div><span>Descrição:</span> {{ $categoria->descricao }}</div>
+
+    <div>
+        <a href="{{ route('categorias.edit', $categoria) }}">Editar</a>
+        <a href="{{ route('categorias.index') }}">Voltar</a>
+    </div>
+</div>
+@endsection
