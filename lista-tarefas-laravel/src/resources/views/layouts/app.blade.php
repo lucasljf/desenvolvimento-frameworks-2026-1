6b1e475a5b7519ad3068c86372f4e0a1d3aa62e8
Sara Farias <fariassara012@gmail.com>
2026-02-27 22:07:24 -0300

    Módulo categoria

--- lista-tarefas-laravel/src/resources/views/layouts/app.blade.php
+++ lista-tarefas-laravel/src/resources/views/layouts/app.blade.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Lista de Tarefas')</title>
+    <title>@yield('title', 'Lista de categorias')</title>
 </head>
 
 <body>
     <div>
         <header>
-            <h1>Lista de Tarefas</h1>
+            <h1>Lista de categorias</h1>
         </header>
     </div>
 
