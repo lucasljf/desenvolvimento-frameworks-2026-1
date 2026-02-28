9cdd8561371a8153ea26e4098332858caca5bc33
Luis Fernando de Oliveira Naves <luis123fernandonaves@gmail.com>
2026-02-27 22:03:16 -0300

    Create CategoriaController

---
+++ lista-tarefas-laravel/src/app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tarefas = Categorias::lasted()->get();
+        return view('categorias.index', compact('categorias'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('.create'); //<<-------- parei aqui
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $dados = $request->validate([
+            'nome' => ['required', 'string'],
+            'cor' => ['nulable', 'string']
+            'descricao' => ['nullable', 'string'],
+        ]);
+
+        Categoria::create($dados);
+
+        return redirect()->route('categorias.index')->with('ok', 'Categoria criada!')
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Categoria $categoria)
+    {
+        return view('categorias.show', compact('categorias'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Categoria $categoria)
+    {
+        return view('categorias.edit', compact('categorias'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Categoria $categoria)
+    {
+        $dados = $request->validate([
+            'nome' => ['required', 'string'],
+            'cor' => ['nulable', 'string']
+            'descricao' => ['nullable', 'string'],
+        ]);
+
+        $categoria->update($dados);
+
+        return redirect()->route('categorias.index')
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Categoria $categoria)
+    {
+        $categoria->delete();
+        return redirect()->route('tarefas.index')-with('ok', 'Tarefas removida');
+    }
+}
