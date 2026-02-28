1d56abcf6eca2615dadcf76f6eb1c96373646ac9
SamuelNM <samuel.nunes1@estudante.ifgoiano.edu.br>
2026-02-27 22:02:57 -0300

    modulo categoria

---
+++ lista-tarefas-laravel/src/database/migrations/create_categoria_tarefas.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->string('cor', 20)->nullable();
+            $table->text('descricao')->nullable();
+            $table->timestamps(); #cria duas colunas: created_at e updated_at
+        });
+    }
+
+ 
+    public function down(): void
+    {
+        Schema::dropIfExists('categorias');
+    }
+};
