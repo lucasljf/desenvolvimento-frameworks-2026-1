5ed3f394a2b715b6e92cfbc0e6f8e2be890ce73a
Luis Fernando de Oliveira Naves <luis123fernandonaves@gmail.com>
2026-02-27 22:06:34 -0300

    Criação das views create, edit, show

---
+++ lista-tarefas-laravel/src/resources/views/categorias/_form.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <div>
+        <label>Título</label>
+        <input name="titulo" value="{{ old('titulo', $tarefa->titulo ?? '') }}" required>
+        @error('titulo') <div>{{ $message }}</div> @enderror
+    </div>
+
+    <div>
+        <label>Descrição</label>
+        <textarea name="descricao" rows="4">{{ old('descricao', $categoria->descricao ?? '') }}</textarea>
+        @error('descricao') <div>{{ $message }}</div> @enderror
+    </div>
+</div>
